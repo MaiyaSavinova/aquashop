d3e9aa4b55bb526e6377962a03afca4016f1cfd1
MaiyaSavinova <hello251127@yandex.ru>
2025-03-10 18:10:43 +0300

    Создание шаблона для выравнивания

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,10 @@
+import Layout from "../components/Layout";
+
 export function Home() {
     return <>
         <h1>Главная страница</h1>
+        <Layout mb={2} dt={1} gap="small"></Layout>
+        <Layout mb={4} dt={2} gap="none"></Layout>
+        <Layout mb={4} dt={4}></Layout>
     </>
 }
